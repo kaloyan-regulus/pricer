95c1c106b015085bca9eda0c25d07e83a553bfa0
Kaloyan Kamburov <kaloyan@webfore.com>
2017-02-09 17:18:46 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router'
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+const appRoutes: Routes = [  
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
